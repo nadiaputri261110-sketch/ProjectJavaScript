423b25e329c849c9683a05cec52adf65b4988c10
nadiaputri261110-sketch <nadiaputri261110@gmail.com>
2026-07-31 08:27:55 +0700

    Add files via upload

---
+++ karyawan.js
@@ -0,0 +1,15 @@
+// Deklarasi karyawan
+const karyawan = {
+    Nama_Karyawan: "Nadia Putri",
+    Umur: 15,
+    Gaji: 13000000,
+    Status_Karyawan: true,
+    Jabatan: null
+};
+
+//template literal
+console.log(`Nama Karyawan: ${karyawan.Nama_Karyawan}`);
+console.log(`Umur: ${karyawan.Umur}`);
+console.log(`Gaji: ${karyawan.Gaji}`);
+console.log(`Status Karyawan: ${karyawan.Status_Karyawan}`);
+console.log(`Jabatan: ${karyawan.Jabatan}`);
